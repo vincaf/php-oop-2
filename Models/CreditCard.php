cb05141bc7eac71057950842998a2d523feca115
vincaf <vincenzocafassog@gmail.com>
2022-09-05 23:40:30 +0200

    feat: add CreditCard model

---
+++ Models/CreditCard.php
@@ -0,0 +1,46 @@
+<?php 
+    include_once __DIR__ . '/User.php';
+
+    class CreditCard extends User{
+        protected $cardNumber;
+        protected $expirationDate;
+        protected $cvv;
+
+        public function __construct($_cardNumber, $_expirationDate, $_cvv){
+            $this->cardNumber = $_cardNumber;
+            $this->setExpirationDate($_expirationDate);
+            $this->cvv = $_cvv;
+        }
+
+        public function getCardNumber(){
+            return $this->cardNumber;
+        }
+
+        public function getExpirationDate(){
+            return $this->expirationDate;
+        }
+
+        public function getCvv(){
+            return $this->cvv;
+        }
+
+        public function setCardNumber($_cardNumber){
+            $this->cardNumber = $_cardNumber;
+        }
+
+        public function setExpirationDate($_expirationDate){
+            try{
+                if($_expirationDate > date('Y-m-d')){
+                    $this->expirationDate = $_expirationDate;
+                } else {
+                    throw new Exception('La data di scadenza non è corretta o la carta è scaduta');
+                }
+            } catch (Exception $e){
+                echo "Exception: " . $e->getMessage();
+            }
+        }
+
+        public function setCvv($_cvv){
+            $this->cvv = $_cvv;
+        }
+    }
