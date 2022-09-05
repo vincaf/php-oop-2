584dc4952f3a0b532afd2fc5f14d5cbd62b29e78
vincaf <vincenzocafassog@gmail.com>
2022-09-06 01:23:34 +0200

    feat: add link to creditCard and new products obj

--- index.php
+++ index.php
@@ -17,15 +17,20 @@
         include_once __DIR__ . "./Models/Toy.php";
         include_once __DIR__ . "./Models/Food.php";
 
-        $user1 = new User("Paolo", "Bitta", "Via Delle Città, Milano");
-        var_dump($user1);
-        $user2 = new RegisteredUser("luca85", "ladirezione", "Luca", "Nervi", "Via Della Vespa, Milano");
-        var_dump($user2);
-
         $creditCard1 = new CreditCard("1111-2222-3333-4444", "10-27", "000");
         var_dump($creditCard1);
         $creditCard2 = new CreditCard("2222-3333-4444-5555", "12-24", "222");
         var_dump($creditCard2);
+
+        $user1 = new User("Paolo", "Bitta", "Via Delle Città, Milano", $creditCard1);
+        var_dump($user1);
+        $user2 = new RegisteredUser("luca85", "ladirezione", "Luca", "Nervi", "Via Della Vespa, Milano", $creditCard2);
+        var_dump($user2);
+
+        $product1 = new Toy("osso", "3", "toys", "gomma", "blue");
+        var_dump($product1);
+        $product2 = new Food("Carne in scatola", "5", "food", "12-2023", "carne di pollo");
+        var_dump($product2);
 
     ?>
 </body>
